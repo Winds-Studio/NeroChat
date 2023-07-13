4fa40a0f4fb8940685132c249dec663cd7457612
kumorio <78680226+ImNotSoftik@users.noreply.github.com>
2023-07-13 22:37:01 +0500

    Update ChatEvent.java

--- src/main/java/me/softik/nerochat/events/ChatEvent.java
+++ src/main/java/me/softik/nerochat/events/ChatEvent.java
@@ -17,7 +17,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 public class ChatEvent implements Listener {
     private final NeroChat plugin;
     
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = false)
     public void onChat(AsyncPlayerChatEvent event) {
         Player chatter = event.getPlayer();
         NeroChatEvent neroChatEvent = new NeroChatEvent(chatter, event.getMessage(), event.isAsynchronous());
